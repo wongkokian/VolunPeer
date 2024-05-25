7254f08200fbb6b96a2ca9b4408bd71c7498f16d
Ian Wong <ianwong106@gmail.com>
2024-05-25 14:13:31 +0800

    initialize project

---
+++ src/test/java/com/project/volunpeer_be/VolunpeerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.volunpeer_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunpeerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
