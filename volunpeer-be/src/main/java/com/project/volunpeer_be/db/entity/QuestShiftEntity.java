3d3381c9d7fc0c0688d978af40cf053b174c89ac
Zhi You <>
2024-05-25 23:32:05 +0800

    rename db collections

--- volunpeer-be/src/main/java/com/project/volunpeer_be/db/entity/QuestShiftEntity.java
+++ volunpeer-be/src/main/java/com/project/volunpeer_be/db/entity/QuestShiftEntity.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document("Quest-Shifts")
+@Document("QuestShifts")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class QuestShiftEntity {
     @Id
