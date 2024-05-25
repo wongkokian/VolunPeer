cc59f29c946b0ad5daaa07ec343e4b8d2c7ed98d
Zhi You <>
2024-05-25 17:45:36 +0800

    modify createQuest and getQuestDetails to account for quest shifts

--- volunpeer-be/src/main/java/com/project/volunpeer_be/db/repository/QuestShiftRepository.java
+++ volunpeer-be/src/main/java/com/project/volunpeer_be/db/repository/QuestShiftRepository.java
@@ -1,9 +1,14 @@
 package com.project.volunpeer_be.db.repository;
 
 import com.project.volunpeer_be.db.entity.QuestShiftEntity;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestShiftRepository extends MongoRepository<QuestShiftEntity, QuestShiftEntity.Key> {
+    // Find all quest shifts based on questId
+    List<QuestShiftEntity> findByQuestId(String questId);
 }
