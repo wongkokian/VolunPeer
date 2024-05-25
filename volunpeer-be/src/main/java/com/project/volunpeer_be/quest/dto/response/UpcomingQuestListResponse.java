9befc09233b39f06f3d5062ae5dee5570576247e
Ian Wong <ianwong106@gmail.com>
2024-05-26 03:15:43 +0800

    add upcoming quests (user) API

---
+++ volunpeer-be/src/main/java/com/project/volunpeer_be/quest/dto/response/UpcomingQuestListResponse.java
@@ -0,0 +1,16 @@
+package com.project.volunpeer_be.quest.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.project.volunpeer_be.common.dto.response.BaseResponse;
+import com.project.volunpeer_be.quest.dto.Quest;
+import lombok.Data;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class UpcomingQuestListResponse extends BaseResponse {
+    private List<Quest> upcomingQuests;
+}
