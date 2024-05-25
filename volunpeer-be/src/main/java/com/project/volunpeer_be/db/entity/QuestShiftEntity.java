5ff02021f210e3823e189c50235ffe41aef2089a
Ian Wong <ianwong106@gmail.com>
2024-05-25 22:20:09 +0800

    shift quest-shift to new collection

--- volunpeer-be/src/main/java/com/project/volunpeer_be/db/entity/QuestShiftEntity.java
+++ volunpeer-be/src/main/java/com/project/volunpeer_be/db/entity/QuestShiftEntity.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document("Quests")
+@Document("Quest-Shifts")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class QuestShiftEntity {
     @Id
--- volunpeer-be/src/main/java/com/project/volunpeer_be/quest/dto/QuestShift.java
+++ volunpeer-be/src/main/java/com/project/volunpeer_be/quest/dto/QuestShift.java
@@ -1,8 +1,10 @@
 package com.project.volunpeer_be.quest.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class QuestShift {
     private Integer shiftNum;
     private String startDateTime;
