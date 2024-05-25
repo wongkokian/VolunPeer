916e41ac2dc981ae57a557851cf1ae34982aad78
Ian Wong <ianwong106@gmail.com>
2024-05-26 05:43:32 +0800

    fix bug

--- volunpeer-be/src/main/java/com/project/volunpeer_be/quest/dto/QuestShift.java
+++ volunpeer-be/src/main/java/com/project/volunpeer_be/quest/dto/QuestShift.java
@@ -1,8 +1,5 @@
 package com.project.volunpeer_be.quest.dto;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
@@ -16,21 +13,4 @@ public class QuestShift {
     private Integer totalSlots;
     private Integer filledSlots;
     private Integer isCompleted;
-
-    public void setStartDateTime(String startDateTime) {
-        this.startDateTime = startDateTime;
-        LocalDateTime localDateTime = LocalDateTime.parse(startDateTime);
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("EEE, MMM dd yyyy");
-        this.date = localDateTime.format(outputFormatter);
-        outputFormatter = DateTimeFormatter.ofPattern("hh:mm a");
-        this.startDateTime = localDateTime.format(outputFormatter);
-    }
-
-    public void setEndDateTime(String endDateTime) {
-        this.endDateTime = endDateTime;
-        LocalDateTime localDateTime = LocalDateTime.parse(endDateTime);
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("hh:mm a");
-        this.endDateTime = localDateTime.format(outputFormatter);
-    }
-
 }
